2814fec4ae57c39f91a9705f9c726d5432d8ff51
carlos-xbm <carlos.xbm@outlook.com>
2024-11-23 17:17:05 -0300

    Adicionado ListaEstado, EditarEstado e RemoverEstado

--- src/main/java/com/projeto/sistema/controle/EstadoControle.java
+++ src/main/java/com/projeto/sistema/controle/EstadoControle.java
@@ -1,5 +1,7 @@
 package com.projeto.sistema.controle;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -7,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import com.projeto.sistema.modelos.Estado;
 import com.projeto.sistema.repositorios.EstadoRepositorio;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,6 +25,19 @@ public class EstadoControle {
         return mv;
     }
 
+    @GetMapping("/listarEstado")
+    public ModelAndView listar() {
+        ModelAndView mv = new ModelAndView("administrativo/estado/lista");
+        mv.addObject("ListaEstados", estadoRepositorio.findAll());
+        return mv;
+    }
+
+    @GetMapping("/editarEstado/{id}")
+    public ModelAndView editar(@PathVariable("id") Long id) {
+        Optional<Estado> estado = estadoRepositorio.findById(id);
+        return cadastrar(estado.get());
+    }
+
     @PostMapping("/salvarEstado")
     public ModelAndView salvar(Estado estado, BindingResult result) {
         if (result.hasErrors()) {
@@ -29,6 +45,13 @@ public class EstadoControle {
         }
         estadoRepositorio.saveAndFlush(estado);
         return cadastrar(new Estado());
+    }
+
+    @GetMapping("/removerEstado/{id}")
+    public ModelAndView remover(@PathVariable("id") Long id) {
+        Optional<Estado> estado = estadoRepositorio.findById(id);
+        estadoRepositorio.delete(estado.get());
+        return listar();
     }
 
 }
