db3f80263679d2d89bccbaaea4d9055c0ee3d575
carlos-xbm <carlos.xbm@outlook.com>
2024-11-24 17:18:12 -0300

    cidade modelo implementado

---
+++ src/main/java/com/projeto/sistema/modelos/Cidade.java
@@ -0,0 +1,28 @@
+package com.projeto.sistema.modelos;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cidade")
+public class Cidade implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String cep;
+
+    @ManyToOne
+    private Estado estado;
+}
