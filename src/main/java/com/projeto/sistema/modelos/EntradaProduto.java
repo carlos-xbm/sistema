d9645540dbf0db842294ad7713144ee22a84a8e8
carlos-xbm <carlos.xbm@outlook.com>
2024-12-15 10:52:26 -0300

    entradaProduto modificado

--- src/main/java/com/projeto/sistema/modelos/EntradaProduto.java
+++ src/main/java/com/projeto/sistema/modelos/EntradaProduto.java
@@ -7,23 +7,36 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "entradaProduto")
+
 public class EntradaProduto implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String obs;
-    private Double valorTotal = 0.00;
-    private Double quantidadeTotal = 0.00;
+    private String valorTotal;
+    private String quantidadeTotal;
     private LocalDate dataEntrada = LocalDate.now();
+
+    @ManyToOne
+    private Produto produto;
+
+    @ManyToOne
+    private ItemEntrada itemEntrada;
+    
+    @ManyToOne
     private Fornecedor fornecedor;
+
+    @ManyToOne
     private Funcionario funcionario;
 
-    
 }
